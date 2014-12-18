e783dd5f183afc922704bced426827d289b7c67b
Debao Zhang <hello@debao.me>
2014-12-18 17:01:58 +0800

    Update the license header of all the source files.

--- src/officeopenxml/opc/opcpackagepart_p.h
+++ src/officeopenxml/opc/opcpackagepart_p.h
@@ -1,15 +1,21 @@
 /****************************************************************************
 **
-** Copyright (C) 2014 Debao Zhang
-** Contact: hello@debao.me
+** Copyright (C) 2014 Debao Zhang <hello@debao.me>
 **
-** This file may be used under the terms of the GNU Lesser
-** General Public License version 2.1 or version 3 as published by the Free
-** Software Foundation and appearing in the file LICENSE.LGPLv21 and
-** LICENSE.LGPLv3 included in the packaging of this file. Please review the
-** following information to ensure the GNU Lesser General Public License
-** requirements will be met: https://www.gnu.org/licenses/lgpl.html and
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+** This library is free software; you can redistribute it and/or modify
+** it under the terms of the GNU Library General Public License version
+** 2.1 or version 3 as published by the Free Software Foundation.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** Library General Public License for more details.
+**
+** You should have received a copy of the GNU Library General Public License
+** along with this library; see the file LICENSE.LGPLv21 and LICENSE.LGPLv3
+** included in the packaging of this file. If not, write to the Free
+** Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+** 02110-1301, USA.
 **
 ****************************************************************************/
 #ifndef OPCPACKAGEPART_P_H
