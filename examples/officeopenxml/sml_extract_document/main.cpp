99b7ea77772da40adfde0abc06c72f7d36953f9e
Debao Zhang <hello@debao.me>
2015-02-23 23:48:30 +0800

    Update license header.

--- examples/officeopenxml/sml_extract_document/main.cpp
+++ examples/officeopenxml/sml_extract_document/main.cpp
@@ -1,3 +1,26 @@
+/****************************************************************************
+**
+** Copyright (C) 2014-2015 Debao Zhang <hello@debao.me>
+**
+** This file is part of the QtOfficeOpenXml library.
+**
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPLv3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or later as published by the Free
+** Software Foundation and appearing in the file LICENSE.GPLv2 included in
+** the packaging of this file. Please review the following information to
+** ensure the GNU General Public License version 2.0 requirements will be
+** met: http://www.gnu.org/licenses/gpl-2.0.html.
+**
+****************************************************************************/
 #include <QtOfficeOpenXml>
 
 using namespace QtOfficeOpenXml;
--- src/officeopenxml/sml/smlchartsheet.h
+++ src/officeopenxml/sml/smlchartsheet.h
@@ -2,20 +2,23 @@
 **
 ** Copyright (C) 2014-2015 Debao Zhang <hello@debao.me>
 **
-** This library is free software; you can redistribute it and/or modify
-** it under the terms of the GNU Library General Public License version
-** 2.1 or version 3 as published by the Free Software Foundation.
+** This file is part of the QtOfficeOpenXml library.
 **
-** This library is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-** Library General Public License for more details.
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPLv3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl.html.
 **
-** You should have received a copy of the GNU Library General Public License
-** along with this library; see the file LICENSE.LGPLv21 and LICENSE.LGPLv3
-** included in the packaging of this file. If not, write to the Free
-** Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-** 02110-1301, USA.
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or later as published by the Free
+** Software Foundation and appearing in the file LICENSE.GPLv2 included in
+** the packaging of this file. Please review the following information to
+** ensure the GNU General Public License version 2.0 requirements will be
+** met: http://www.gnu.org/licenses/gpl-2.0.html.
 **
 ****************************************************************************/
 #ifndef QTOFFICEOPENXML_SML_CHARTSHEET_H
