2cb5f3d0f7496f5125e94240e6cfa8e803cbd5c7
Oleksii Gaienko <galexsoftware@gmail.com>
2023-06-18 22:18:03 +0300

    Fixed an odd precise comparison.
    It's probably better to use a comparison with defined precision or use
    special functions.

--- examples/officeopenxml/opc_package_viewer/binedit.cpp
+++ examples/officeopenxml/opc_package_viewer/binedit.cpp
@@ -1294,7 +1294,7 @@ QString BinEdit::toolTip(const QHelpEvent *helpEvent) const
         asDouble(selStart, doubleValueOld, true);
         str << tableRowStartC << tr("<i>double</i>&nbsp;value:") << numericTableRowSepC
             << doubleValue << tableRowEndC;
-        if (doubleValue != doubleValueOld)
+        if (qFuzzyCompare(doubleValue, doubleValueOld))
             str << tableRowStartC << tr("Previous <i>double</i>&nbsp;value:") << numericTableRowSepC
                 << doubleValueOld << tableRowEndC;
         str << "</table>";
@@ -1308,7 +1308,7 @@ QString BinEdit::toolTip(const QHelpEvent *helpEvent) const
         asFloat(selStart, floatValueOld, true);
         str << tableRowStartC << tr("<i>float</i>&nbsp;value:") << numericTableRowSepC
             << floatValue << tableRowEndC;
-        if (floatValue != floatValueOld)
+        if (qFuzzyCompare(floatValue, floatValueOld))
             str << tableRowStartC << tr("Previous <i>float</i>&nbsp;value:") << numericTableRowSepC
                 << floatValueOld << tableRowEndC;
 
